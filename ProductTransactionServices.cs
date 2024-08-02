471e766f2dddce9f12b6b4dc18f86366a0ef3dd2
chingxsss <163365879+chingxsss@users.noreply.github.com>
2024-08-02 21:56:40 +0800

    Add files via upload

---
+++ ProductTransactionServices.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Model;
+using DataLayer;
+
+namespace BusinessLogic
+{
+    public class ProductTransactionServices
+    {
+        private readonly ProductRepository productRepository = new ProductRepository();
+
+        public List<Product> GetAllProducts()
+        {
+            return productRepository.GetProducts();
+        }
+
+        public void AddProduct(Product product)
+        {
+            productRepository.AddProduct(product);
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            productRepository.UpdateProduct(product);
+        }
+
+        public void DeleteProduct(int productId)
+        {
+            productRepository.DeleteProduct(productId);
+        }
+    }
+} 
+
---
+++ UserValidationServices.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using DataLayer;
+using Model;
+
+namespace BusinessLogic
+{
+    public class UserValidationServices
+    {
+        private readonly ProductRepository productRepository = new ProductRepository();
+
+        public bool CheckIfUserExists(string username, string password)
+        {
+            var user = productRepository.GetUsers().FirstOrDefault(u => u.Username == username && u.Password == password);
+            return user != null;
+        }
+    }
+}
